303bf2ddd6134ae6519b5f0d70f7be24c2f258b8
Jeb Watson <jeb.watson@gmail.com>
2023-04-08 15:56:14 -0500

    Call the redirection service and provider service

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Redirector;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Starting redirection service...");
+
+        var redirectionService = new RedirectionService();
+        
+        // TODO: Parse json into list
+        var redirectsJson = RedirectProviderService.GetRedirectsJson();
+        var redirects = RedirectProviderService.GetRedirectsList();
+
+        foreach (var redirect in redirects)
+        {
+            redirectionService.HandleRedirect(redirect);
+        }
+
+        System.Console.WriteLine("All redirects processed...");
+        System.Console.WriteLine("Exiting...");
+    }
+}
