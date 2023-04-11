239c9fb9e337a2f55ee0b73363923b8052fc366d
Jeb Watson <jeb.watson@gmail.com>
2023-04-10 20:58:13 -0500

    Add logging via NLog

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
-﻿using Redirector;
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Redirector;
 
 internal class Program
 {
     private static async Task Main(string[] args)
     {
+        var config = new LoggingConfiguration();
+        var consoleTarget = new ConsoleTarget
+        {
+            Name = "console",
+            Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}",
+        };
+
+        config.AddRule(LogLevel.Debug, LogLevel.Fatal, consoleTarget, "*");
+        LogManager.Configuration = config;
+
+        var logger = LogManager.GetCurrentClassLogger();
         var cacheRefresh = TimeSpan.FromMinutes(2);
 
-        Console.WriteLine("Starting redirection service...");
+        logger.Info("Starting redirection service...");
 
         var redirectionService = new RedirectionService();
         
@@ -17,10 +31,12 @@ internal class Program
             //var redirectsJson = RedirectProviderService.GetRedirectsJson();
             var redirects = RedirectProviderService.GetRedirectsList();
 
-            System.Console.WriteLine("Processing requests...");
+            logger.Info("Processing requests...");
             await redirectionService.HandleRedirectsAsync(redirects);
 
+            logger.Info("Awaiting next cache refresh...");
             await Task.Delay(cacheRefresh);
+            logger.Info("Refreshing cache...");
         }
     }
 }
